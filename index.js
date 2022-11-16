c1c130e4cf066bd7b3e40dd05345c483e19711d1
Kirill Kalachev <kalachev.kirill@bk.ru>
2022-11-16 19:56:41 +0300

    Promise chain

--- index.js
+++ index.js
@@ -18,3 +18,36 @@ loadScript('./one.js')
 	two();
 	three();
 })
+
+
+//response to the server
+function loadJson(url) {
+	return fetch(url)
+	.then(response => response.json());
+}
+
+function loadUserGithub(name) {
+	return fetch(`https://api.github.com/users/${name}`)
+	.then(response => response.json());
+}
+
+function showAvatar(githubUser) {
+	return new Promise(function(resolve, reject) {
+		let img = document.createElement('img');
+		img.src = githubUser.avatar_url;
+		img.className = "user-icon";
+		document.body.appendChild(img);
+	
+		setTimeout(() => {
+			img.remove();
+			resolve(githubUser);
+		}, 3000);
+	});
+}
+
+loadJson('./user.json')
+.then(user => loadUserGithub(user.name))
+.then(githubUser => showAvatar(githubUser))
+.then(githubUser => {
+	alert(`Закончили показ ${githubUser.name}`);
+})
