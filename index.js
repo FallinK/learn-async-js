0c8efba4f19033e17456ac71b377c0de3d55b6ff
Kirill Kalachev <kalachev.kirill@bk.ru>
2022-11-16 16:19:56 +0300

    Promise base

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function loadScript(src, callback) {
-	let script = document.createElement('script');
-	script.src = src;
-	script.onload = () => callback();
-	document.head.append(script);
-}
-
-loadScript('./tmp.js', () => {
-	printLog();
+let promise = new Promise(function(resolve, reject) {
+	setTimeout(() => resolve("done"), 1000);
 });
+
+// let promise = new Promise(function(resolve, reject) {
+// 	setTimeout(() => reject(new Error("Error result")), 1000);
+// });
+
+promise.then (result => alert(result))
+.catch(error => alert(error))
+.finally(/*Метсто для остановки лоадеров, закрытия сессий и др.*/);
