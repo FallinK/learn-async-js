4e3f7c5c7a71cc629840e41205593ea80099159c
Kirill Kalachev <kalachev.kirill@bk.ru>
2022-11-16 16:23:50 +0300

    Callback use

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function loadScript(src, callback) {
 	let script = document.createElement('script');
 	script.src = src;
-	script.onload = () => callback();
+
+	script.onload = () => callback(null, script);
+	script.onerror = () => callback (new Error(`Ошибка загрузки скрипта ${src}`));
+
 	document.head.append(script);
 }
 
