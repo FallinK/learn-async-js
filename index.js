cf0fbfd6a3e60511496e7634e30f841359525857
Kirill Kalachev <kalachev.kirill@bk.ru>
2022-11-18 15:06:12 +0300

    promisification

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+function promisify(f, manyArgs = false) {
+	return function(...args) {
+		return new Promise((resolve, reject) => {
+			function callback(err, ...results) {
+				if(err) reject(err)
+				else resolve(manyArgs ? results : results[0]);
+
+			}
+			args.push(callback);
+
+			f.call(this, ...args);
+		})
+	}
+}
+
+
 function loadScript(src, callback) {
 	let script = document.createElement('script');
 	script.src = src;
-
+	
 	script.onload = () => callback(null, script);
-	script.onerror = () => callback (new Error(`Ошибка загрузки скрипта ${src}`));
+	script.onerror = () => callback(new Error(`Ошибка загрузки скрипта ${src}`), script);
 
 	document.head.append(script);
 }
 
-loadScript('./tmp.js', () => {
-	printLog();
-});
+let newLoadScriptPromise = promisify(loadScript);
+newLoadScriptPromise('./tmp.js').then(
+	script => printLog(script),
+	err => alert(err.message)
+);
+
+
+// let loadScriptPromise = function(src) {
+// 	return new Promise((resolve, reject) => {
+// 		loadScript(src, (err, script) => {
+// 			if(err) reject(err)
+// 			else resolve(script);
+// 		})
+// 	})
+// }
+// loadScriptPromise('./tmp1.js').then(
+// 	script => printLog(script),
+// 	err => alert(err.message)
+// );
